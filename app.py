dcc8f1bfb5e8a68c555b0cbf96def79759f7fdef
Drew Galbraith <ragalbra@asu.edu>
2025-07-08 17:46:28 -0400

    Remove dotenv usage for Streamlit Cloud

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import numpy as np
 import streamlit as st
 from sentence_transformers import SentenceTransformer
 import anthropic
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
 # Claude API key
 # api_key = os.getenv("ANTHROPIC_API_KEY")
